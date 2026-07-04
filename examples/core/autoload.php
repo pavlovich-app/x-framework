364376f3ad33178dde73a1c435d8e1e6d6de6c11
Oleksandr <P>
2026-07-04 21:02:57 +0200

    next.

--- examples/core/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @param $classname
- * @throws Exception
- */
-spl_autoload_register(function($classname){
-    $filename = ROOT_DIR.str_replace('\\', '/', $classname).".php";
-    include_once($filename);
-});
